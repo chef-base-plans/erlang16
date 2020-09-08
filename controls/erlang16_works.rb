fe71b18782897f0e7dafde2d3f6dd1d3373ffda5
Gavin Didrichsen <gavin.didrichsen@gmail.com>
2020-09-08 19:42:32 +0100

    Remove stderr tests
    Signed-off-by: Gavin Didrichsen <gavin.didrichsen@gmail.com>

--- controls/erlang16_works.rb
+++ controls/erlang16_works.rb
@@ -28,14 +28,12 @@ control 'core-plans-erlang16-works' do
   describe plan_installation_directory do
     its('exit_status') { should eq 0 }
     its('stdout') { should_not be_empty }
-    its('stderr') { should be_empty }
   end
   
   plan_pkg_version = plan_installation_directory.stdout.split("/")[5]
   describe command("cat #{plan_installation_directory.stdout.chomp!}/lib/erlang/releases/RELEASES") do
     its('exit_status') { should eq 0 }
     its('stdout') { should match /#{plan_pkg_version}/ }
-    its('stderr') { should be_empty }
   end
   
   fullset = {
@@ -48,16 +46,13 @@ control 'core-plans-erlang16-works' do
       exit_pattern: /^[0]$/,
     },
     "epmd" => {
-      io: "stderr", 
     },
     # "erl" => {
     #   command_prefix: "echo 'io:format(erlang:system_info(otp_release)). erlang:halt(0).' | hab pkg exec core/erlang16 -- ",
     #   command_suffix: "",
     #   command_output_pattern: /#{plan_pkg_version}/,
-    #   io: "stderr",
     # },
     "erlc" => {
-      io: "stderr", 
     },
     "escript" => {
       command_suffix: "",
@@ -70,10 +65,8 @@ control 'core-plans-erlang16-works' do
       END
     },
     "run_erl" => {
-      io: "stderr",
     },
     "to_erl" => {
-      io: "stderr",
     },
     "typer" => {
       command_suffix: "--help",
@@ -82,10 +75,9 @@ control 'core-plans-erlang16-works' do
   }.each do |binary_name, value|
     # set default values if each binary_name doesn't define an over-ride
     command_prefix = value[:command_prefix] || ""
-    command_suffix = value[:command_suffix] || "-help"
+    command_suffix = value.has_key?(:command_suffix) ? "#{value[:command_suffix]} 2>&1" : "-help 2>&1"
     command_output_pattern = value[:command_output_pattern] || /[uU]sage:.+#{binary_name}/ 
     exit_pattern = value[:exit_pattern] || /^[^0]$/ # use /^[^0]$/ for non-zero exit status
-    io = value[:io] || "stdout"
     script = value[:script]
 
     # set default 'command_under_test' only adding a Tempfile if 'script' is defined
@@ -104,7 +96,7 @@ control 'core-plans-erlang16-works' do
     # verify output
     describe command_under_test do
       its('exit_status') { should cmp exit_pattern }
-      its(io) { should match command_output_pattern }
+      its("stdout") { should match command_output_pattern }
     end
   end
 end
